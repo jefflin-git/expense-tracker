36d174ab54feea8f96b66bbab5dcfac2134e31da
jefflin <linjef0711@gmail.com>
2020-12-28 16:03:32 +0800

    feat: set app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+// 載入 express 並建構應用程式伺服器
+const express = require('express')
+const app = express()
+const PORT = 3000
+//載入express-handlebars
+const exphbs = require('express-handlebars')
+// 引用body-parser
+const bodyParser = require('body-parser')
+//載入method-override
+const methodOverride = require('method-override')
+
+//設定express-handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+//設定body-parser
+app.use(bodyParser.urlencoded({ extended: true }))
+
+//設定method-override
+app.use(methodOverride('_method'))
+
+app.get('/', (req, res) => {
+  res.send('test')
+})
+
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT} at ${Date()}`)
+})
