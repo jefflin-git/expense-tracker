9e81823f9d87ec559897b1ce3bff019742be3819
jefflin <linjef0711@gmail.com>
2021-01-18 16:09:50 +0800

    feat: modify seeder

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,17 +1,45 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const db = require('../../config/mongoose')
 const Record = require('../record')
+const User = require('../user')
 const seedData = require('../../seedData')
+const bcrypt = require('bcryptjs')
 const lists = seedData.records
-const records = []
+const users = seedData.seedUsers
 
 db.once('open', () => {
   console.log('mongodb connected!')
-  lists.forEach(list => {
-    records.push(list)
+  users.forEach((user, index) => {
+    bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(user.password, salt))
+      .then(hash => User.create({ name: user.name, email: user.email, password: hash }))
+      .then(async (user) => {
+        switch (index) {
+          case 0:
+            const record1 = []
+            for (let i = 0; i <= 2; i++) {
+              lists[i].userId = user._id
+              record1.push(lists[i])
+            }
+            await Record.insertMany(record1)
+            break
+
+          case 1:
+            const record2 = []
+            for (let i = 3; i <= 4; i++) {
+              lists[i].userId = user._id
+              record2.push(lists[i])
+            }
+            await Record.insertMany(record2)
+            break
+        }
+      })
+      .then(() => {
+        console.log('all done!')
+        process.exit()
+      })
   })
-  Record.create(records)
-    .then(() => {
-      db.close()
-      console.log("record done!")
-    })
 })
