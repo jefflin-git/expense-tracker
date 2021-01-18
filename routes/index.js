84c8621f5a32fd681dba8ffae25d1b6d23c2bfe0
jefflin <linjef0711@gmail.com>
2021-01-18 09:49:29 +0800

    feat: add user authentication in record and home routers

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,14 @@ const home = require('./modules/home')
 //引入record模組程式碼
 const record = require('./modules/record')
 const users = require('./modules/users')
+const { authenticator } = require('../middleware/auth')
 
-//將網址結構符合 / 字串的request導向home模組
-router.use('/', home)
+
 //將網址結構符合 /record 字串的request導向record模組
-router.use('/record', record)
+router.use('/record', authenticator, record)
 router.use('/users', users)
+//將網址結構符合 / 字串的request導向home模組
+router.use('/', authenticator, home)
 
 //匯出路由器
 module.exports = router
